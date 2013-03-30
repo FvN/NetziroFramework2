e2a426856cc8935c9f5d946a8c19bce5935ac5a2
Alessio Nobile <netziro@gmail.com>
2013-03-30 16:24:10 +0100

    adding a db profile example

---
+++ core/database/profiles/Profile1.profile.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+* ----------------------------------------------------------------------
+*                 NETZIRO FRAMEWORK DATABASE PROFILE FILE
+* ----------------------------------------------------------------------
+* FILE RELATIVE LOCATION:	core/database/profiles/Profile1.profile.php
+* CREATOR:					Alessio Nobile
+* CREATING DATA:			23/04/12
+* ----------------------------------------------------------------------
+* FILE DESCRIPTION:			
+* ----------------------------------------------------------------------
+* TRACKING  LOG - LOG YOUR CHANGES ONLY IF YOU ARE DOING IMPORTANT UPDATES ( CHANGE OF METHOD, ADDING/DELETING LINES OF CODE, BUGFIX)
+* ----------------------------------------------------------------------
+* UPDATE : 
+* MODDER: ALESSIO NOBILE / DATE AND HOUR : 02/11/2011 - 12:45
+* ----------------------------------------------------------------------
+*/
+
+$credentials[ "hostname" ] = "localhost";
+$credentials[ "database" ] = "test";
+$credentials[ "username" ] = "root";
+$credentials[ "password" ] = "ph03nix";
+
