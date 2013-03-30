aefc62511c811cfe0770f32c4f06def1855b9d9a
Alessio Nobile <netziro@gmail.com>
2013-03-30 16:23:47 +0100

    NFDatabase class improving

--- core/database/NFDatabase.class.php
+++ core/database/NFDatabase.class.php
@@ -22,7 +22,7 @@
 * AUTHOR Alessio Nobile >> www.netziro.it >> netziro@gmail.com
 * ----------------------------------------------------------------------
 * CLASS NAME:				NFDatabase
-* FILE RELATIVE LOCATION:	framework/NFDatabase.class.php
+* FILE RELATIVE LOCATION:	core/database/NFDatabase.class.php
 * CREATOR:					Alessio Nobile
 * ----------------------------------------------------------------------
 * CLASS DESCRIPTION:		METHODS WITHIN CLASS USED FOR COMMUNICATION WITH DATABASE.
@@ -55,6 +55,7 @@ class NFDatabase {
 	protected $ssl;			// SSL Link
 	protected $database_type;
 	protected $encoding;
+	protected $lock = false;
 	public $database; 		// Database Name			|
 	public $link;			// Database Link ID			| This variable will contain the ID of MySQL connection you started
 	public $query;			// Query String				| This variable will contain the last MySQL string you sent
@@ -63,26 +64,50 @@ class NFDatabase {
 	public $id;
 	public $errors = array();
 	
-
 	/**
 	 * @desc
 	 * Loading the class you will set variables needed for MySQL link
-	 * Remember to call openLinkDB(), for database link start, after the new class statement.
+	 * Remember to call openLinkDB(), for database link to start, after the new class statement.
+	 * 
+	 * **** CREDENTIALS ARRAY EXAMPLE:
+	 * 
+	 * ****	$credentials[ "profile" ] = "ProfileName"; If Profile is set, it will be load the specified profile file overriding the array keys below.
+	 * 
+	 * ****	$credentials[ "hostname" ] = "mysql.database.com";
+	 * ****	$credentials[ "database" ] = "MyDatabaseName";
+	 * ****	$credentials[ "username" ] = "root";
+	 * ****	$credentials[ "password" ] = "MyPasswords";
 	 *
-	 * @param string $hostname	- You will pass Hostname String Value
-	 * @param string $database	- You will pass Database String Value
-	 * @param string $username	- You will pass Username String Value
-	 * @param string $password	- You will pass Password String Value
+	 * @param array 	$credentials	You will pass an array containing all database credentials
+	 * @param string 	$timezone		TimeZone string - Default : Europe/Amsterdam
+	 * @param boolean	$ssl			Do you want instanciate a ssl link? - Default : false
+	 * @param string	$mysql			Define the database type - Default : mysql
+	 * @param string	$encoding		Define the session's encoding - Default : utf8
+	 * 	 
 	 */
-	function NFDatabase( $hostname, $database, $username, $password, $timezone = "Europe/Amsterdam", $ssl = true, $database_type = "mysql", $encoding = "utf8" ) {
+	function NFDatabase( $credentials = array(), $timezone = "Europe/Amsterdam", $ssl = false, $database_type = "mysql", $encoding = "utf8" ) {
 		
 		try{
 		
+			// ------------------------------------- | START Check the DB Profile existence
+			if( isset( $credentials[ "profile" ] ) ){
+				if( !empty( $credentials[ "profile" ] ) ){
+					
+					$profile_file = __DIR__ . "/profiles/" . $credentials[ "profile" ] . ".profile.php";
+					
+					if( file_exists( $profile_file ) ){ 
+						require_once( $profile_file );
+					} else { throw new Exception( "You have specified a db profile, but the file has been not found" ); }
+					
+				}
+			}
+			// ------------------------------------- | END
+			
 			// ------------------------------------- | START Check core variables existence
-			if( !empty( $hostname ) ){ $this->hostname = $hostname; } else { throw new Exception( "You must specify the hostname" ); }
-			if( !empty( $database ) ){ $this->database = $database; } else { throw new Exception( "You must specify the database name" ); }
-			if( !empty( $username ) ){ $this->username = $username; } else { throw new Exception( "You must specify an username" ); }
-			if( !empty( $password ) ){ $this->password = $password; } else { throw new Exception( "You must specify a password" ); }
+			if( !empty( $credentials[ "hostname" ] ) ){ $this->hostname = $credentials[ "hostname" ]; } else { throw new Exception( "You must specify the hostname" ); }
+			if( !empty( $credentials[ "database" ] ) ){ $this->database = $credentials[ "database" ]; } else { throw new Exception( "You must specify the database name" ); }
+			if( !empty( $credentials[ "username" ] ) ){ $this->username = $credentials[ "username" ]; } else { throw new Exception( "You must specify an username" ); }
+			if( !empty( $credentials[ "password" ] ) ){ $this->password = $credentials[ "password" ]; } else { throw new Exception( "You must specify a password" ); }
 			// ------------------------------------- | END
 			
 			// ------------------------------------- | START Set secondary importance database variables
@@ -92,7 +117,15 @@ class NFDatabase {
 			$this->encoding = $encoding;
 			// ------------------------------------- | END
 			
-		} catch( Exception $e ){ $e->getMessage(); return false; }
+		} catch( Exception $e ){
+			 
+			// ------------------------------------- | START Store the error and return false
+			$this->errors[ ] = $e->getMessage(); 
+			$this->lock = true; 
+			return false; 
+			// ------------------------------------- | END
+			
+		}
 		
 	}
 	
@@ -108,7 +141,7 @@ class NFDatabase {
 	 * FALSE if link will be unsuccessful
 	 * 
 	 **** PDO Instance example
-	 **** ------------------
+	 **** -------------------------------------------------------------------
 	 **** $PDO = new PDO( "mssql:host=$host;dbname=$dbname", $user, $pass );  
 	 **** $PDO = new PDO( "sybase:host=$host;dbname=$dbname", $user, $pass );
 	 **** $PDO = new PDO( "mysql:host=$host;dbname=$dbname", $user, $pass );  
@@ -119,22 +152,33 @@ class NFDatabase {
 		
 		try{
 			
-			// ------------------------------------- | START PDO Instance
-			$this->link = new PDO( "$this->database_type:host=$this->hostname;dbname=$this->database", $this->username, $this->password );
-			// ------------------------------------- | END  
+			if( !$this->lock ){
+				
+				// ------------------------------------- | START PDO Instance
+				$this->link = new PDO( "$this->database_type:host=$this->hostname;dbname=$this->database", $this->username, $this->password );
+				// ------------------------------------- | END  
+				
+				// ------------------------------------- | START Define instance settings
+				$this->link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+				$this->link->query( "SET CHARACTER SET $this->encoding" );
+				$this->link->query( "SET NAMES '$this->encoding'" );
+				$this->link->query( "SET SESSION time_zone = '$this->timezone'" );
+				// ------------------------------------- | END
+				
+				// ------------------------------------- | START Return
+				return true;
+				// ------------------------------------- | END
+				
+			} else { throw new PDOException( "The database class is locked" ); return false; }
 			
-			// ------------------------------------- | START Define instance settings
-			$this->link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-			$this->link->query( "SET CHARACTER SET $this->encoding" );
-			$this->link->query( "SET NAMES '$this->encoding'" );
-			$this->link->query( "SET SESSION time_zone = '$this->timezone'" );
+		} catch( PDOException $e ){ 
+			
+			// ------------------------------------- | START Store the error and return false
+			$this->errors[ ] = 'Line: ' . $e->getLine() . ' ' . $e->getMessage(); 
+			return false; 
 			// ------------------------------------- | END
-			
-			// ------------------------------------- | START Return
-			return true;
-			// ------------------------------------- | END
-			
-		} catch( PDOException $e ){ $this->errors[] = 'Line: '.$e->getLine().' '.$e->getMessage(); return false; }
+		
+		}
 		
 	} 
 	
@@ -150,32 +194,38 @@ class NFDatabase {
 	 * Calling this method you will send a query string to MySQL Database
 	 * 
 	 * @param string $query	- You will pass the MySQL string
+	 * @param array $params	- Array containing params list that needs to be binded on the query by PDO
 	 * 
 	 * @return resource - Method will return query results
 	 */
-	public function Query( $query ){
+	public function Query( $query, $params = array( ) ){
 		
 		try{
 			
 			// ------------------------------------- | START Sanityze the query
 			$query = trim( $query );
-			$this->query = $this->link->quote( $query );
+			$this->query = $this->link->prepare( $query );
 			// ------------------------------------- | END
 			
 			// ------------------------------------- | START Query executing
-			$result = null;
-			$result = $this->link->query( $this->query );
+			$result = $this->query->execute( $params );
 			// ------------------------------------- | END
 			
 			// ------------------------------------- | START Results fetch
-			$this->result = $result->fetchAll();
+			if( $result ){ 
+				
+				// ------------------------------------- | START Fetch results
+				$this->result = $this->query->fetchAll();
+				// ------------------------------------- | END
+
+				// ------------------------------------- | START Define some info
+				if( preg_match( "/^SELECT/", $this->query ) OR preg_match( "/^SHOW/", $this->query ) ){ $this->rows = count( $this->result );	}
+				if( preg_match( "/^INSERT/", $this->query ) ){ $this->id = $this->link->lastInsertId(); }
+				// ------------------------------------- | END
+				
+			} else { throw new PDOException( "Something wrong on the query" ); }
 			// ------------------------------------- | END
 
-			// ------------------------------------- | START Define some info
-			if( preg_match( "/^SELECT/", $this->query ) OR preg_match( "/^SHOW/", $this->query ) ){ $this->rows = count( $this->result );	}
-			if( preg_match( "/^INSERT/", $this->query ) ){ $this->id = $this->link->lastInsertId(); }
-			// ------------------------------------- | END
-				
 			// ------------------------------------- | START Variables cleaning up
 			unset( $query );
 			unset( $result );
@@ -185,8 +235,31 @@ class NFDatabase {
 			return $this->result;
 			// ------------------------------------- | END
 			
-		} catch( PDOException $e ){ $this->errors[] = 'Line: '.$e->getLine().' '.$e->getMessage(); return false; }
+		} catch( PDOException $e ){ 
+			
+			// ------------------------------------- | START Store the error and return false
+			$this->errors[ ] = 'Line: ' . $e->getLine() . ' ' . $e->getMessage(); 
+			return false; 
+			// ------------------------------------- | END
+		
+		}
 		
 	}
+	
+	/**
+	 * @desc
+	 * Calling this method you will return the errors array
+	 * 
+	 * @return array
+	 */
+	public function GetErrors( ){ if( !empty( $this->errors ) ){ return $this->errors; } else { return false; } }
+	
+	/**
+	 * @desc
+	 * Calling this method you will print the errors array
+	 * 
+	 * @return array
+	 */
+	public function PrintErrors( ){ if( !empty( $this->errors ) ){ print_r ( $this->errors ); } else { return false; } }
 	
 }
--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+
+
+require_once( "core/database/NFDatabase.class.php" );
+
+$credentials[ "profile" ] = "Profile1";
+/*$credentials[ "database" ] = "test";
+$credentials[ "username" ] = "root";
+$credentials[ "password" ] = "ph03nix";*/
+
+$NFDatabase = new NFDatabase( $credentials );
+$linkopened = $NFDatabase->OpenLink();
+
+if( $linkopened === true ){
+	
+	echo "Socket opened";
+
+	$sql = "SELECT * FROM `test` WHERE `test` = :param1";
+	$params = array( ':param1' => "d'alessio" );
+	$results = $NFDatabase->Query( $sql, $params );
+	
+	print_r( $results );
+	
+	$NFDatabase->PrintErrors();
+	
+} else {
+	
+	echo "Socket not opened";
+	
+	$NFDatabase->PrintErrors();
+	
+}
+
